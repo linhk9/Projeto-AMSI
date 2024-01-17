ec41c65ab4724da09d3070b447879310aa79f567
Filipe Marques <2211921@my.ipleiria.pt>
2024-01-17 18:57:52 +0000

    Uso da API no Registo do Utilizador

---
+++ app/src/main/java/pt/ipleiria/estg/dei/lojacalcado/utils/LojaJsonParser.java
@@ -0,0 +1,43 @@
+package pt.ipleiria.estg.dei.lojacalcado.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class LojaJsonParser {
+    public static String parserJsonLogin(JSONObject response) {
+        String msg = "";
+
+        try {
+            msg = response.getString("message");
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        return msg;
+    }
+
+    public static String parserJsonRegisto(JSONObject response) {
+        String msg = "";
+
+        try {
+            msg = response.getString("message");
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        return msg;
+    }
+
+    public static Boolean isConnectionInternet(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+        return (ni != null && ni.isConnected());
+    }
+}
