3d201b1f778715b8205e1b877f4acc174097243e
Rodrigo Luís <54328638+linhk9@users.noreply.github.com>
2024-01-17 19:01:22 +0000

    added menu lateral

--- app/src/main/java/pt/ipleiria/estg/dei/lojacalcado/MenuMainActivity.java
+++ app/src/main/java/pt/ipleiria/estg/dei/lojacalcado/MenuMainActivity.java
@@ -1,14 +1,77 @@
 package pt.ipleiria.estg.dei.lojacalcado;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MenuMainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MenuMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private FragmentManager fragmentManager;
+    public static final String EMAIL = "EMAIL";
+    public static final int ADD = 100, EDIT = 200, DELETE = 300;
+    public static final String OP_CODE = "op_detalhes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navView);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.ndOpen, R.string.ndClose);
+        toggle.syncState();
+        drawer.addDrawerListener(toggle);
+
+        carregarCabecalho();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        fragmentManager = getSupportFragmentManager();
+
+        carregarFragmentoInicial();
     }
+
+    private boolean carregarFragmentoInicial() {
+        Menu menu = navigationView.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+        return onNavigationItemSelected(menuItem);
+    }
+
+    private void carregarCabecalho() {
+        Intent intent = getIntent();
+        SharedPreferences sharedPreferencesEmailUser = getSharedPreferences("DADOS_USER", MODE_PRIVATE);
+
+        View hview = navigationView.getHeaderView(0);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
